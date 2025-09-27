47a51da68917e9069b33da98b51f5b399b71360d
Jenoah Kers <hello@jenoah.nl>
2025-09-27 22:00:25 +0200

    Added division for Vector3f

--- src/main/java/nl/framegengine/core/utils/Calculus.java
+++ src/main/java/nl/framegengine/core/utils/Calculus.java
@@ -50,6 +50,22 @@ public class Calculus {
         return new Vector3f(a.x - b.x, a.y - b.y, a.z - b.z);
     }
 
+    public static Vector3i divideVectors(Vector3i a, Vector3i b){
+        return new Vector3i(a.x / b.x, a.y / b.y, a.z / b.z);
+    }
+
+    public static Vector3f divideVectors(Vector3f a, Vector3i b){
+        return new Vector3f(a.x / b.x, a.y / b.y, a.z / b.z);
+    }
+
+    public static Vector3f divideVectors(Vector3f a, Vector3f b){
+        return new Vector3f(a.x / b.x, a.y / b.y, a.z / b.z);
+    }
+
+    public static Vector3f divideVectors(Vector3i a, Vector3f b){
+        return new Vector3f(a.x / b.x, a.y / b.y, a.z / b.z);
+    }
+
     public static Vector3i multiplyVector(Vector3i a, float b){
         return new Vector3i((int)(a.x * b), (int)(a.y * b), (int)(a.z * b));
     }
